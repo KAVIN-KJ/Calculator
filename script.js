56a89f0f06cff9754f859d9e384577b7abe9dbcd
KAVIN RAJA <kavinkj2004@gmail.com>
2024-02-18 21:28:42 +0530

    main

---
+++ script.js
@@ -0,0 +1,37 @@
+let expression = "0";
+let answer = "0"
+        function updateDisplay() {
+            document.getElementById('expression').value = expression;
+        }
+
+        function clearInput() {
+            expression = "0";
+            updateDisplay();
+        }
+
+        function backSpace() {
+            if (expression.length === 1) {
+                expression = "0";
+            } else {
+                expression = expression.slice(0, -1);
+            }
+            updateDisplay();
+        }
+
+        function handleInput(value) {
+            if (expression === "0") {
+                expression = value;
+            } else {
+                expression += value;
+            }
+            updateDisplay();
+        }
+
+        function operate() {
+                const result = eval(expression);
+                answer = result.toString();
+                console.log(result)
+                console.log(result.toString())
+                expression = result.toString();
+                updateDisplay();
+        }
